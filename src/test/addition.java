10764bd70157084d85a6f577d8df4281d155f165
nickaggelidis26 <nickaggelidis26@gmail.com>
2025-05-19 08:57:03 +0300

    Initial commit

---
+++ src/test/addition.java
@@ -0,0 +1,13 @@
+package test;
+
+public class addition {
+
+    public static void main(String[] args) {
+
+        System.out.println(addition(5,7));
+    }
+
+    public static int addition (int a, int b){
+        return a+b;
+    }
+}
