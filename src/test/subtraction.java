5d873e929b7a483b7f65b674f274836e5e366a18
nickaggelidis26 <nickaggelidis26@gmail.com>
2025-05-19 09:01:38 +0300

    feat: add subtraction class

---
+++ src/test/subtraction.java
@@ -0,0 +1,12 @@
+package test;
+
+public class subtraction {
+
+    public static void main(String[] args) {
+
+        System.out.println(subtract(7,2));
+    }
+    public static int subtract(int a, int b){
+        return a-b;
+    }
+}
